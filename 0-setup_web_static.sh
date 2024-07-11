bcfcded228f7a74cf6bbef6bcfedea635281043e
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-07-11 14:29:48 -0700

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -49,45 +49,8 @@ sudo bash -c "cat <<EOF > /data/web_static/releases/test/index.html
 $index
 EOF"
 
-# Creat the 404 page
-err='<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 Not Found</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            padding: 50px;
-        }
-        h1 {
-            color: #333;
-        }
-        p {
-            color: #666;
-        }
-        footer {
-            margin-top: 50px;
-            font-style: italic;
-            color: #999;
-        }
-    </style>
-</head>
-<body>
-    <h1>404 Not Found</h1>
-    <p>The requested URL was not found on this server.</p>
-    <footer>Aksaim Mohamed Amin</footer>
-</body>
-</html>'
-
-sudo bash -c "cat <<EOF > /data/web_static/releases/test/404.html
-$err
-EOF"
-
 # Updte Nginx config
-config='server {
+config="server {
        listen 80 default_server;
        listen [::]:80 default_server;
 
@@ -99,13 +62,7 @@ config='server {
        location /hbnb_static {
        		alias /data/web_static/current/;
        }
-
-       error_page 404 /404.html;
-       location = /404.html {
-       root /var/www/html;
-       internal;
-       }
-}'
+}"
 
 sudo bash -c "cat <<EOF > /etc/nginx/sites-available/default
 $config
