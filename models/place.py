d5ac11c6bdfa75810d79c10b90f37b5b5f7def67
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-09-07 10:58:54 -0700

    DBStorage - Amenity

--- models/place.py
+++ models/place.py
@@ -2,8 +2,17 @@
 """ Place Module for HBNB project """
 import os
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False)
+)
 
 
 class Place(BaseModel, Base):
@@ -23,6 +32,8 @@ class Place(BaseModel, Base):
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship('Review', cascade='all, delete', backref='place')
+        amenities = relationship('Amenity', backref='place_amenities',
+                                 secondary='place_amenity', viewonly=False)
     else:
         @property
         def reviews(self):
@@ -32,3 +43,24 @@ class Place(BaseModel, Base):
             """
             reviews = storage.all(Review).values()
             return [review for review in reviews if self.id == review.id]
+
+        @property
+        def amenities(self):
+            """
+            returns the list of Amenity instances based on the attribute
+            amenity_ids that contains all Amenity.id linked to the Place
+            """
+            amenities = storage.all(Amenity)
+
+            amenities_list = []
+            for key, value in amenities.items():
+                if key in self.amenity_ids:
+                    amenities_list.append(amenities[key])
+
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """Set amenity ids"""
+            if isInstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
