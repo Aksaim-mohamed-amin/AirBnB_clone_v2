a2d8288ea77f1b2927845d110253267d185b0f27
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-09-11 06:38:52 -0700

    distributes an archive to your web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""distributes an archive to web servers"""
+
+from fabric.api import env, put, run
+import os
+
+env.hosts = ['100.25.157.136', '54.237.118.245']
+
+
+def do_deploy(archive_path):
+    """distributes an archive to web servers"""
+    if not os.path.exists(archive_path):
+        return False
+
+    tgz_file = archive_path.split('/')[1]
+    folder = tgz_file.split('.')[0]
+
+    try:
+        put(archive_path, f"/tmp/")
+
+        run(f"sudo mkdir -p /data/web_static/releases/{folder}/")
+
+        run(f"sudo tar -xzf /tmp/{tgz_file} \
+        -C /data/web_static/releases/{folder}/")
+
+        run(f"sudo rm -rf /tmp/{tgz_file}")
+
+        run(f"sudo rsync  /data/web_static/releases/{folder}/web_static/* \
+        /data/web_static/releases/{folder}/")
+
+        run(f"sudo rm -rf /data/web_static/releases/{folder}/web_static")
+
+        run(f"sudo rm -rf /data/web_static/current")
+
+        run(f"sudo ln -s /data/web_static/releases/{folder}/ \
+        /data/web_static/current")
+
+    except Exception as e:
+        print('Error:', e)
+        return False
+
+    print('New version deployed!')
+    return True
