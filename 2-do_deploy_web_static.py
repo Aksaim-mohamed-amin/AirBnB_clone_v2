0de28a37e3a9deda0cc2340f158a0e4e617cb2af
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-09-12 09:06:31 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,49 +2,27 @@
 """distributes an archive to web servers"""
 
 from fabric.api import env, put, run
-import os
+from os.path import exists
 
 env.hosts = ['100.25.157.136', '54.237.118.245']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """Deploy a new releas to the server"""
-
-    # Check if the archive file exists
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
-    archive = archive_path.split('/')[1].split('.')[0]
-
     try:
-        # Upload the archive to the server
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-
-        #Uncompress the archive
-        run('sudo mkdir -p /data/web_static/releases/{}'.format(archive))
-        run('sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
-            .format(archive, archive))
-
-        # Delete the archive from the web server
-        run('sudo rm -rf /tmp/{}.tgz'.format(archive))
-
-        # Move the static files to the version file
-        run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'
-            .format(archive, archive))
-        run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(archive))
-
-        # Delete the old symbolic link from the server
-        run('sudo rm -rf /data/web_static/current')
-
-        # Create a new symbolic link
-        run('sudo ln -sf /data/web_static/releases/{}/ /data/web_static/current'
-            .format(archive))
-
-    except Exception as e:
-        print(e)
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
         return False
-
-    print('New version deployed!')
-    return True
