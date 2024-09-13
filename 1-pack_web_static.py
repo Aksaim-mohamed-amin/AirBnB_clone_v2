57516785bfb4e22563ae9c44b65f6201661378b7
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-09-13 09:37:01 -0700

    task 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""Generate a .tgz archive from the current content of web_static"""
+"""Compress web static package
+"""
 from fabric.api import local
 from datetime import datetime
-import os
 
 
 def do_pack():
-    """
-    generates a .tgz archive from the contents of the web_static folder
-    of AirBnB Clone repo
-    """
-    # Create the versions directory
-    if not os.path.exists('versions'):
-        os.mkdir('versions')
+    """Function to compress directory
 
-    # Get the current time stamp and create the archive name
+    Return: path to archive on success; None on fail
+    """
+    # Get current time
     now = datetime.now()
-    archive_name = f"versions/web_static_{now.strftime('%y%m%d%H%M%S')}.tgz"
+    now = now.strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_' + now + '.tgz'
 
-    # Create the .tgz archive for the web_static folder
-    print(f"Packing web_static to {archive_name}")
-    result = local(f"tar -cvzf {archive_name} web_static")
+    # Create archive
+    local('mkdir -p versions/')
+    result = local('tar -cvzf {} web_static/'.format(archive_path))
 
-    if result.failed:
-        return None
-    return archive_name
+    # Check if archiving was successful
+    if result.succeeded:
+        return archive_path
+    return None
---
+++ old/1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Generate a .tgz archive from the current content of web_static"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """
+    generates a .tgz archive from the contents of the web_static folder
+    of AirBnB Clone repo
+    """
+    # Create the versions directory
+    if not os.path.exists('versions'):
+        os.mkdir('versions')
+
+    # Get the current time stamp and create the archive name
+    now = datetime.now()
+    archive_name = f"versions/web_static_{now.strftime('%y%m%d%H%M%S')}.tgz"
+
+    # Create the .tgz archive for the web_static folder
+    print(f"Packing web_static to {archive_name}")
+    result = local(f"tar -cvzf {archive_name} web_static")
+
+    if result.failed:
+        return None
+    return archive_name
