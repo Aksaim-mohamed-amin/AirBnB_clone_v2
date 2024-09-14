90cfcd57a17576757f8dd9b09835ba6c3ef738e1
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-09-14 11:54:43 -0700

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,79 +1,98 @@
 #!/usr/bin/python3
-"""Compress web static package
-"""
-from fabric.api import env, local, put, run
+"""Compress and deploy web static package."""
+from fabric import task
 from datetime import datetime
 import os
 
-env.hosts = ['100.25.157.136', '54.237.118.245']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+# Define the hosts, user, and key file globally
+env = {
+    'hosts': ['100.25.157.136', '54.237.118.245'],
+    'user': 'ubuntu',
+    'key_filename': '~/.ssh/school'
+}
 
+@task
+def do_pack(c):
+    """Create a tar archive of the web_static directory.
 
-def do_pack():
-    """Function to compress directory
-    Return: path to archive on success; None on fail
+    Args:
+        c (Connection): Fabric connection object.
+
+    Returns:
+        str: Path to the created archive on success.
+        None: If the archive creation fails.
     """
-    # Get current time
-    now = datetime.now()
-    now = now.strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_' + now + '.tgz'
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_path = f'versions/web_static_{now}.tgz'
 
-    # Create archive
-    local('mkdir -p versions')
-    result = local('tar -cvzf {} web_static/'.format(archive_path))
+    try:
+        # Create the versions directory if it does not exist
+        c.local('mkdir -p versions')
 
-    # Check if archiving was successful
-    if result.succeeded:
-        return archive_path
+        # Create the tar archive
+        result = c.local(f'tar -cvzf {archive_path} web_static/', capture=True)
+        if result.succeeded:
+            return archive_path
+    except Exception as e:
+        print(f'Error creating archive: {e}')
+
     return None
 
+@task
+def do_deploy(c, archive_path):
+    """Distribute an archive to web servers.
 
-def do_deploy(archive_path):
-    """distributes an archive to web servers"""
+    Args:
+        c (Connection): Fabric connection object.
+        archive_path (str): The path to the archive to deploy.
+
+    Returns:
+        bool: True if the deployment was successful, False otherwise.
+    """
     if not os.path.exists(archive_path):
+        print(f'Archive path {archive_path} does not exist.')
         return False
 
-    folder = archive_path.split('/')[1].split('.')[0]
+    folder = os.path.basename(archive_path).split('.')[0]
 
     try:
         # Upload the archive to /tmp/
-        put(archive_path, '/tmp/')
+        c.put(archive_path, '/tmp/')
 
-        # Uncompress the archive
-        run('sudo mkdir -p /data/web_static/releases/{}'
-            .format(folder))
+        # Create directory for the new release
+        c.run(f'sudo mkdir -p /data/web_static/releases/{folder}')
 
-        run('sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
-            .format(folder, folder))
+        # Uncompress the archive to the release folder
+        c.run(f'sudo tar -xzf /tmp/{folder}.tgz -C /data/web_static/releases/{folder}')
 
-        # Remove the archive
-        run('sudo rm /tmp/{}.tgz'.format(folder))
+        # Remove the archive from /tmp/
+        c.run(f'sudo rm /tmp/{folder}.tgz')
 
-        # Create a new the symbolic link
-        run('sudo mv /data/web_static/releases/{}/web_static/* \
-        /data/web_static/releases/{}'.format(folder, folder))
+        # Move files and clean up
+        c.run(f'sudo mv /data/web_static/releases/{folder}/web_static/* /data/web_static/releases/{folder}')
+        c.run(f'sudo rm -rf /data/web_static/releases/{folder}/web_static')
 
-        run('sudo rm -rf /data/web_static/releases/{}/web_static'
-            .format(folder))
-
-        run('sudo rm -rf /data/web_static/current')
-
-        run('sudo ln -s /data/web_static/releases/{}/ \
-        /data/web_static/current'.format(folder, folder))
+        # Update the symbolic link
+        c.run(f'sudo ln -sf /data/web_static/releases/{folder} /data/web_static/current')
 
     except Exception as e:
-        print('Error:', e)
+        print(f'Error deploying archive: {e}')
         return False
 
     print('New version deployed!')
     return True
 
+@task
+def deploy(c):
+    """Create an archive and deploy it to the web servers.
 
-def deploy():
-    """Creates and distributes an archive to web server"""
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
+    Args:
+        c (Connection): Fabric connection object.
 
-    return do_deploy(archive_path)
+    Returns:
+        bool: True if the deployment was successful, False otherwise.
+    """
+    archive_path = do_pack(c)
+    if archive_path:
+        return do_deploy(c, archive_path)
+    return False
